6db49a6a1330643bf046bf3b3442d0503b24e1c9
AtomicBool <raymon8331@gmail.com>
2026-01-24 20:50:05 -0500

    update README.md

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 ```bash
 git clone https://github.com/FRCCriticalCircuits/RobotCode2026.git
 ```
-Please turn off `detect indentation` in your vscode settings and use 4 spaces for TAB
+# vscode settings
+disable `detect indentation`
+enable `git: auto fetch`
--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "RobotCode2026";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 15;
-	public static final String GIT_SHA = "28ba7cb8fae82d06cfff6511e15129eb8bbae696";
-	public static final String GIT_DATE = "2026-01-24 10:26:01 EST";
+	public static final int GIT_REVISION = 17;
+	public static final String GIT_SHA = "0fefd6371af285eda2601c0057bf541ea0dbbe28";
+	public static final String GIT_DATE = "2026-01-24 20:28:03 EST";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2026-01-24 20:25:54 EST";
-	public static final long BUILD_UNIX_TIME = 1769304354225L;
+	public static final String BUILD_DATE = "2026-01-24 20:46:55 EST";
+	public static final long BUILD_UNIX_TIME = 1769305615163L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants(){}
