39920634115e38bcd7ddec8497e517aaf2d6e927
Romgi Productions <JonathanGraydon22@gmail.com>
2026-01-24 20:26:34 -0500

    Updated to WPILib version 2026.2.1

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "RobotCode2026";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 13;
-	public static final String GIT_SHA = "74848dbcf43520bff5ee5c5165c21d6ec2fc2fed";
-	public static final String GIT_DATE = "2026-01-23 23:53:13 EST";
+	public static final int GIT_REVISION = 15;
+	public static final String GIT_SHA = "28ba7cb8fae82d06cfff6511e15129eb8bbae696";
+	public static final String GIT_DATE = "2026-01-24 10:26:01 EST";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2026-01-24 10:21:00 EST";
-	public static final long BUILD_UNIX_TIME = 1769268060041L;
+	public static final String BUILD_DATE = "2026-01-24 20:25:54 EST";
+	public static final long BUILD_UNIX_TIME = 1769304354225L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants(){}
