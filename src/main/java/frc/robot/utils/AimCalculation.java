317bc82a98d4df663caee5376015b2d397fb49f4
atomicbool <raymon8331@gmail.com>
2026-02-14 00:21:16 -0500

    replace getStateCopy() with getState()

--- src/main/java/frc/robot/utils/AimCalculation.java
+++ src/main/java/frc/robot/utils/AimCalculation.java
@@ -1,5 +1,29 @@
 package frc.robot.utils;
 
+import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
+
+import edu.wpi.first.math.Pair;
+import frc.robot.subsystems.drive.Drive;
+
 public class AimCalculation {
-    
+    private final Drive drive;
+    private static final double shotTime = 0.0;
+
+    public AimCalculation(Drive drive){
+        this.drive = drive;
+    }
+
+    public Pair<Double, Double> getAimParams(){
+        SwerveDriveState state = drive.getState();
+        
+        // double newX = state.Pose.getMeasureX().baseUnitMagnitude() + (shotTime * state.Speeds.vxMetersPerSecond);
+        // double newY = state.Pose.getMeasureY().baseUnitMagnitude() + (shotTime * state.Speeds.vyMetersPerSecond);
+
+
+
+        double yaw = 0.0;
+        double pitch = 0.0;
+
+        return new Pair<>(yaw, pitch);
+    }
 }
