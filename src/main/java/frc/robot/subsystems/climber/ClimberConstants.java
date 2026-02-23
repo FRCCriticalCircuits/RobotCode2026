fd496b9f6ca9ef186fadaa29aaee5f5b3315f13e
Le Minh <leminh2606bn@gmail.com>
2026-02-23 11:12:20 -0500

    ClimberIOSim update

--- src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -1,6 +1,15 @@
 package frc.robot.subsystems.climber;
 
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+
 public class ClimberConstants {
+    public static final DCMotor CLIMBER_GEARBOX = DCMotor.getNeoVortex(1);
+    public static final LinearSystem<N2, N1, N2> CLIMBER_STATE_SPACE = LinearSystemId.createDCMotorSystem(0.1, 0.01);
+    
     public class HAL{
         public static final double CLIMBER_GEARING = 1.0;
 
--- src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -1,7 +1,68 @@
 package frc.robot.subsystems.climber;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
 public class ClimberIOSim implements ClimberIO{
+    private final DCMotorSim climber;
+
+    private final PIDController climberController = new PIDController(10, 0, 0);
+
+    private double appliedVoltsClimber = 0;
+    private double climberVelocity = 0;
+    private boolean climberStopped = true;
+
     public ClimberIOSim(){
-        
+        climber = new DCMotorSim(
+            ClimberConstants.CLIMBER_STATE_SPACE,
+            ClimberConstants.CLIMBER_GEARBOX
+        );
     }
+
+    @Override
+    public void updateInputs(ClimberIOInputs inputs) {
+        climber.update(0.02);
+        inputs.climberPosition = climber.getAngularPositionRad();
+        inputs.climberVelocity = climber.getAngularVelocityRadPerSec();
+
+        inputs.climberAppliedVolts = appliedVoltsClimber;
+        inputs.climberCurrentAmps = climber.getCurrentDrawAmps();
+
+        inputs.climberConnected = true;
+
+        if(!climberStopped){
+            this.appliedVoltsClimber = climberController.calculate(
+                climber.getAngularVelocityRadPerSec(),
+                this.climberVelocity
+            );
+
+            climber.setInputVoltage(appliedVoltsClimber);
+        }
+    }
+    @Override
+    public Command runClimber(double velocity) {
+        return Commands.run(
+            () -> {
+                this.climberStopped = false;
+                this.climberVelocity = velocity;
+            }
+        ).withName("climber.runClimberVelocity");
+    }
+
+    @Override
+    public void runClimberVoltage(double voltage) {
+        this.climberStopped = false;
+        this.appliedVoltsClimber = voltage;
+        climber.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void stopMotors() {
+        this.climberStopped = true;
+        this.appliedVoltsClimber = 0;
+
+        climber.setInputVoltage(0);
+    }    
 }
