762141516f730e4019962dd7b08f3231559c9418
Le Minh <leminh2606bn@gmail.com>
2026-02-28 09:44:14 -0500

    Climber Implementation

--- src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.climber;
 
-import edu.wpi.first.math.controller.PIDController;
+//import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -8,17 +8,11 @@ import edu.wpi.first.wpilibj2.command.Commands;
 public class ClimberIOSim implements ClimberIO{
     private final DCMotorSim climber;
 
-    private final PIDController climberController = new PIDController(10, 0, 0);
+    //private final PIDController climberController = new PIDController(10, 0, 0);
 
     private double appliedVoltsClimber = 0;
-    private double climberVelocity = 0;
     private boolean climberStopped = true;
 
-    /**
-     * @deprecated
-     * runClimber Function is suppose to take voltage as input; We dont need velocity closeloop, we need either position closeloop or voltage openloop
-     */
-    @Deprecated()
     public ClimberIOSim(){
         climber = new DCMotorSim(
             ClimberConstants.CLIMBER_STATE_SPACE,
@@ -29,27 +23,21 @@ public class ClimberIOSim implements ClimberIO{
     @Override
     public void updateInputs(ClimberIOInputs inputs) {
         climber.update(0.02);
-        inputs.climberPosition = climber.getAngularPositionRad();
-
         inputs.climberConnected = true;
-
+        
         if(!climberStopped){
-            this.appliedVoltsClimber = climberController.calculate(
-                climber.getAngularVelocityRadPerSec(),
-                this.climberVelocity
-            );
-
+            this.appliedVoltsClimber = climber.getInputVoltage();
             climber.setInputVoltage(appliedVoltsClimber);
-        }
+        }   
     }
     @Override
-    public Command runClimber(double velocity) {
+    public Command runClimber(double voltage) {
         return Commands.run(
             () -> {
                 this.climberStopped = false;
-                this.climberVelocity = velocity;
+                this.appliedVoltsClimber = voltage;
             }
-        ).withName("climber.runClimberVelocity");
+        ).withName("climber.runClimberVoltage");
     }
 
     @Override
