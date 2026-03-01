f5475c1864e04c849ffaa2cf928b3abc2b0cd12a
AtomicBool <raymon8331@gmail.com>
2026-03-01 01:16:32 -0500

    fix build error by remove AutoLog for Vision

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,15 +1,14 @@
 package frc.robot.subsystems.vision;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.vision.VisionIO.VisionIOInputs;
 
 public class Vision extends SubsystemBase{
     private final VisionIO[] listVisionIO;
     private final Drive drivetrain;
 
-    private final VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
+    private final VisionIOInputs inputs = new VisionIOInputs();
 
     public Vision(Drive drivetrain, VisionIO... visionIOs){
         this.drivetrain = drivetrain;
@@ -20,7 +19,6 @@ public class Vision extends SubsystemBase{
     public void periodic() {
         for (VisionIO visionIO : listVisionIO) {
             visionIO.updateInputs(inputs);
-            Logger.processInputs("VisionResult/" + visionIO.toString(), inputs);
 
             for (var observedData : inputs.poseObservation) {
                 drivetrain.addVisionMeasurement(
--- src/main/java/frc/robot/subsystems/vision/VisionIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -2,12 +2,9 @@ package frc.robot.subsystems.vision;
 
 import java.util.List;
 
-import org.littletonrobotics.junction.AutoLog;
-
 import edu.wpi.first.math.geometry.Pose2d;
 
 public interface VisionIO {
-    @AutoLog
     public static class VisionIOInputs {
         public boolean connected = false;
         public List<PoseObservation> poseObservation = null;
