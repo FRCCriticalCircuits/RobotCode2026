1d963eac7d113faa87765c0ca8bd277730752b53
Le Minh <leminh2606bn@gmail.com>
2026-03-22 11:30:33 -0400

    Trajectory calculate

--- src/main/java/frc/robot/utils/calc/CalculatorConstants.java
+++ src/main/java/frc/robot/utils/calc/CalculatorConstants.java
@@ -1,6 +1,7 @@
 package frc.robot.utils.calc;
 
 import java.util.Map;
+import java.util.function.Supplier;
 
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import frc.robot.subsystems.shooter.ShooterConstants.TUNING;
@@ -8,6 +9,7 @@ import frc.robot.subsystems.shooter.ShooterConstants.TUNING;
 public class CalculatorConstants {
     // TODO tune for actual robot
     public static final double shotTime = 0.3;
+    public static final Supplier<Double> distanceRobotToTarget = () -> 0.0;
 
     // TODO tune-LUT
     // max 0.11, min 0.005 for rotations
@@ -15,13 +17,13 @@ public class CalculatorConstants {
     public static InterpolatingDoubleTreeMap hoodAngle = InterpolatingDoubleTreeMap.ofEntries(
         Map.entry(0.0, TUNING.HOOD_MIN_POSITION_RAD),
         Map.entry(3.0, TUNING.HOOD_MIN_POSITION_RAD),
-        Map.entry(8.0, TUNING.HOOD_MAX_POSITION_RAD) // passing
+        Map.entry(8.0, TUNING.HOOD_MAX_POSITION_RAD) 
     );
 
     public static InterpolatingDoubleTreeMap shooterVelocity = InterpolatingDoubleTreeMap.ofEntries(
-        Map.entry(1.7, 42.3 * Math.PI * 2),    
-        Map.entry(1.953, 43.3 * Math.PI * 2),
-        Map.entry(3.04, 49.5 * Math.PI * 2)
+        Map.entry(0.0, AimCalc.calculateRequiredVelocity(0.0, TUNING.HOOD_MIN_POSITION_RAD)),    
+        Map.entry(3.0, AimCalc.calculateRequiredVelocity(3.0, TUNING.HOOD_MIN_POSITION_RAD)),
+        Map.entry(8.0, AimCalc.calculateRequiredVelocity(8.0, TUNING.HOOD_MAX_POSITION_RAD))
     );
 
     // TODO decide limit (in degress) of the cone-shape check for climbing calculator
